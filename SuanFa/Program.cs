3660dae6b09a1389be5d54def49c3d32d6bc2998
liboran <535889004@qq.com>
2023-09-02 19:28:27 +0800

    9.2

--- SuanFa/Program.cs
+++ SuanFa/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+var v = new SuanFa.剑指29();
+var m =new  int[3][];
+m[0] = new int[3] { 1, 2, 3 };
+m[1] = new int[3] { 4, 5, 6 };
+m[2]= new int[3] {7,8, 9 };
+foreach(var a in v.SpiralOrder(m))
+{
+    Console.WriteLine(a);
+}
